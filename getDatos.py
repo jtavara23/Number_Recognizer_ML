e00e5d0eabd932b080c18ea6f5f5a3f435d86485
Josue G. Tavara Idrogo <joshuatavara23@gmail.com>
2017-10-19 15:28:06 -0500

    New way of visualize conv plots

--- getDatos.py
+++ getDatos.py
@@ -1,28 +1,29 @@
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 import os
-
+from funcionesAuxiliares import plot_conv_weights, plot_conv_layer,display
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-path = '/media/josuetavara/Gaston/mnist/mnistDS/'
+path = '/media/josuetavara/Gaston/signals/models5/'
+
 with tf.Session() as sess:
 	sess.run(tf.global_variables_initializer())
-	saver = tf.train.import_meta_graph(path + 'CNN/models/model-100.meta')
-	saver.restore(sess, tf.train.latest_checkpoint(path + 'CNN/models/.'))
-	print "Modelo restaurado",tf.train.latest_checkpoint(path + 'CNN/models/.')
+	saver = tf.train.import_meta_graph(path + 'model-49040.meta')
+	saver.restore(sess, tf.train.latest_checkpoint(path + '.'))
+	print "Modelo restaurado",tf.train.latest_checkpoint(path + '.')
 	
 	
 	all_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES)
-	#print all_vars
-	print "tensors: ",len(all_vars)
-	"""
+	#print "tensors: ",len(all_vars)
+	#print sess.run(all_vars[0])[4][4]
+	#print sess.run(all_vars[8])[1023]
+	
 	for ind in xrange(0,len(all_vars)):
-		print all_vars[ind]
-	"""
+		print ind,all_vars[ind]
+		#print sess.run(all_vars[ind])
 	
-	x = all_vars[0]
-	print x 
-	print sess.run(x)
-	x = all_vars[2]
-	print x 
-	print sess.run(x)
-	
+	#plot_conv_weights(sess.run(all_vars[0]),8,4,0)
+	#plot_conv_weights(sess.run(all_vars[2]),8,8,0)
+#"""
